c91d4ee56d50e021fc29f120fca6e7afd2f70fdb
mohamadamin <mamin75.dev@gmail.com>
2022-08-20 22:48:10 +0430

    change template to bootstrapmade personal site template

--- app/main.py
+++ app/main.py
@@ -56,6 +56,7 @@ def main():
         educations.append(edu)
 
     return render_template("index.html", data={"profile": profile, "experiences": expriences, "educations": educations})
+    # return render_template("index.html")
 
 
 if __name__ == "__main__":
